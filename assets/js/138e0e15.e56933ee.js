66bed3eb0b031fd925cd05d6407b5f40cddba8eb
JulBea <114906036+JulBea@users.noreply.github.com>
2024-10-02 11:50:00 +0200

    Delete assets directory

--- assets/js/138e0e15.e56933ee.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkz_documentation=self.webpackChunkz_documentation||[]).push([[921],{1597:e=>{e.exports=JSON.parse('{"name":"@easyops-cn/docusaurus-search-local","id":"default"}')}}]);
